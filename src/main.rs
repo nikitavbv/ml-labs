cc19322dd666be0b5c3aaafe7830af6f7a4d82ae
Nikita Volobuev <nikitavbv@gmail.com>
2023-01-23 17:28:50 +0200

    feat(npz): load data into tensors

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,40 @@ use {
 fn main() {
     println!("Hello, world!");
 
-    /*let mut npz = NpzReader::new(File::open("./mnist.npz").unwrap()).unwrap();
-    let names = npz.names().unwrap();
-
-    let x_train: Array4<f32> = npz.by_name("x_train.npy").unwrap();*/
-
     let mut npz = npyz::npz::NpzArchive::open("./mnist.npz").unwrap();
-    let x_test = x_dataset_from_npz(&mut npz, "x-test");
+    let x_train = x_dataset_from_npz(&mut npz, "x_train");
+    let x_test = x_dataset_from_npz(&mut npz, "x_test");
+    let y_train = y_dataset_from_npz(&mut npz, "y_train");
+    let y_test = y_dataset_from_npz(&mut npz, "y_test");
 
     println!("result: {:?}", x_test);
+}
+
+fn y_dataset_from_npz(npz: &mut NpzArchive<BufReader<File>>, name: &str) -> Vec<Tensor> {
+    let npy = npz.by_name(name).unwrap().unwrap();
+
+    let shape = npy.shape().to_vec();
+    println!("y shape is: {:?}", shape);
+
+    dataset_to_tensor(
+        npy.data().unwrap().map(|v| v.unwrap()).collect(),
+        shape[0],
+        shape[1]
+    )
 }
 
 fn x_dataset_from_npz(npz: &mut NpzArchive<BufReader<File>>, name: &str) -> Vec<Tensor> {
     let npy = npz.by_name(name).unwrap().unwrap();
 
     let shape = npy.shape().to_vec();
-    x_dataset(
+    dataset_to_tensor(
         npy.data().unwrap().map(|v| v.unwrap()).collect(), 
         shape[0], 
         shape[1] * shape[2]
     )
 }
 
-fn x_dataset(data: Vec<u8>, total_entries: u64, tensor_len: u64) -> Vec<Tensor> {
+fn dataset_to_tensor(data: Vec<u8>, total_entries: u64, tensor_len: u64) -> Vec<Tensor> {
     let mut res = Vec::new();
     for i in 0..total_entries {
         let start = (i * tensor_len) as usize;
